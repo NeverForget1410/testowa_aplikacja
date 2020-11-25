a50d4b6ca280c7fbfe85090b043e5c6336d07773
NeverForget1410 <p-figura@wp.pl>
2020-11-25 17:17:10 +0100

    Initial commit

---
+++ src/main/java/figura/testowa_aplikacja/TestowaAplikacjaApplication.java
@@ -0,0 +1,13 @@
+package figura.testowa_aplikacja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestowaAplikacjaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestowaAplikacjaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/figura/testowa_aplikacja/TestowaAplikacjaApplicationTests.java
@@ -0,0 +1,13 @@
+package figura.testowa_aplikacja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestowaAplikacjaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
